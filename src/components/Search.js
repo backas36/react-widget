aa953f5db2ba81ee068bddb2e88d4f6ac0c13fd3
Ashi <backas36@gmail.com>
2021-09-26 15:50:37 +0800

    finished search

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,11 +4,22 @@ import axios from 'axios'
 
 const Search = () => {
   const [term, setTerm] = useState('programming')
+  const [debouncedTerm, setDebouncedTerm] = useState(term)
   const [results, setResults] = useState([])
 
-  useEffect(() => {
-    console.log('Inital render or term was changed')
 
+  useEffect(() => {
+
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term)
+    }, 1000)
+
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [term])
+
+  useEffect(() => {
     const search = async () => {
       const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
@@ -16,25 +27,17 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term
+          srsearch: debouncedTerm,
         }
       })
       setResults(data.query.search)
     }
-    if (term && !results.length) {
+    if (debouncedTerm) {
       search()
-    } else {
-      const timeoutId = setTimeout(() => {
-        console.log('timesup')
-        if (term) search()
-      }, 1000)
-      return () => {
-        clearTimeout(timeoutId)
-      }
     }
+  }, [debouncedTerm])
 
 
-  }, [term])
 
   const renderedResults = results.map(result => {
     return (
